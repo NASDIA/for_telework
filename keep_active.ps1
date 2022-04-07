e211c8abed3f7e9ad52b1305cad045ec6d2f6e23
NASDIA <103134938+NASDIA@users.noreply.github.com>
2022-04-07 19:43:21 +0900

    Add files via upload

--- keep_active.ps1
+++ keep_active.ps1
@@ -7,34 +7,18 @@ Add-Type -AssemblyName System.Windows.Forms
 # ----------------------------------------
 # Private
 # ----------------------------------------
-function p_str_send_key( $str_data )
-{
-	$char_data_array = $str_data.ToCharArray()
-	
-	foreach ( $char_data in $char_data_array )
-	{
-		[System.Windows.Forms.SendKeys]::SendWait( $char_data )
-		sleep -m 500
-	}
-}
 
 # ----------------------------------------
 # Main
 # ----------------------------------------
-for( $loop = 0; $loop -lt 1; ++$loop )
+for( $loop = 0; $loop -lt 2; ++$loop )
 {
 	Invoke-Item C:\Windows\notepad.exe
 	sleep 3
 	
-	p_str_send_key( "Hello`nWorld." );
+	[System.Windows.Forms.SendKeys]::SendWait( "Hello`nWorld." )
+	sleep 3
 	
 	taskkill -F -IM notepad.exe
+	sleep 4
 }
-
-# ----------------------------------------
-# memo
-# ----------------------------------------
-# Get-ExecutionPolicy
-# Set-ExecutionPolicy RemoteSigned
-# Set-ExecutionPolicy Restricted
-# Set-ExecutionPolicy RemoteSigned -Scope Process -Force
